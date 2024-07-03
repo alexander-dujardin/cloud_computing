db4f2053169e9ccb8d66ef918b047c92454149ac
Alexander Dujardin <alexander.dujardin@outlook.com>
2024-07-03 14:27:26 +0200

    format main.js

--- frontend-backend/public/main.js
+++ frontend-backend/public/main.js
@@ -6,41 +6,34 @@ function uploadImage(uploadZone) {
   const file = fileInput.files[0];
 
   if (file) {
-      const formData = new FormData();
-      formData.append(`image${uploadZone}`, file);
-      fetch(`/upload/${uploadZone}`, {
-              method: "POST",
-              body: formData,
-          })
-          .then((response) => response.json())
-          .catch((error) => {
-              console.error("Error:", error);
-          });
+    const formData = new FormData();
+    formData.append(`image${uploadZone}`, file);
+    fetch(`/upload/${uploadZone}`, {
+        method: "POST",
+        body: formData,
+      })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   } else {
-      console.error("No file selected.");
-      alert("Select a file before uploading.");
+    console.error("No file selected.");
+    alert("Select a file before uploading.");
   }
 }
 
+function setMiniatureView(elementId, base64Image) {
+  document.getElementById(elementId).src = `data:image/png;base64,${base64Image}`;
+}
 
-socket.on("displayMiniatureView1", (base64Image) => {
-  document.getElementById(
-    "miniatureView1"
-).src = `data:image/png;base64,${base64Image}`;
-});
-socket.on("displayMiniatureView2", (base64Image) => {
-  document.getElementById(
-    "miniatureView2"
-).src = `data:image/png;base64,${base64Image}`;
-});
-socket.on("displayMiniatureView3", (base64Image) => {
-  document.getElementById(
-    "miniatureView3"
-).src = `data:image/png;base64,${base64Image}`;
+const uploadZoneIds = [1, 2, 3]; // Range of IDs
+
+uploadZoneIds.forEach(id => {
+  socket.on(`displayMiniatureView${id}`, (base64Image) => {
+    setMiniatureView(`miniatureView${id}`, base64Image);
+  });
 });
 
 socket.on("imageUploaded", (data) => {
-  document.getElementById(
-      `miniatureView${data.upload_zone}`
-  ).src = `data:image/png;base64,${data.image}`;
+  setMiniatureView(`miniatureView${data.upload_zone}`, data.image);
 });
